3dea6e465ff9ab431a63fd377d917c130cae4a6a
Raafa1993 <rafaelajds@gmail.com>
2023-05-16 21:42:48 -0300

    config server and web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900 p-6">
+      <h1 className="font-bold text-zinc-50">Sua cápsula do tempo</h1>
+    </div>
+  )
+}
